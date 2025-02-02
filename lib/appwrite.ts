fea0dbaf1a4397f7d1e82fbabceb22f0abe7677e
adzaky <muhadlim17@gmail.com>
2025-02-02 20:06:43 +0800

    feat(appwrite): add database and collection IDs to appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,22 +1,28 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases, OAuthProvider } from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
   platform: "com.adzaky.restate",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
 
 client
   .setEndpoint(config.endpoint!)
-  .setProject(config.project!)
+  .setProject(config.projectId!)
   .setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const database = new Databases(client);
 
 export async function login() {
   try {
